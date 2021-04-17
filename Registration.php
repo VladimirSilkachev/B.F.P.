4275de26f00a49a7bd789823f11e05dd776a0398
KirillPopov001 <55420132+KirillPopov001@users.noreply.github.com>
2021-04-17 16:49:52 +0700

    Create Registration.php

---
+++ Registration.php
@@ -0,0 +1,61 @@
+<?php
+php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<form id="head" action="new_acc.php" method="post" class="ramka">
+
+    <div>
+        <label id="login" for="login">Login</label>
+        <input type="text" name="login">
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input type="password" name="password">
+    </div>
+
+    <div>
+        <label for="name">Name</label>
+        <input type="text" name="name">
+    </div>
+
+    <div>
+        <label for="surname">Surname</label>
+        <input type="text" name="surname">
+    </div>
+
+    <div>
+        <label for="birth">Date of birth</label>
+        <input type="text" name="birth">
+    </div>
+
+    <div>
+        <label for="address">Address</label>
+        <input type="text" name="address">
+    </div>
+
+    <div>
+        <label for="sex">Sex</label>
+        <input type="text" name="sex">
+    </div>
+
+    <div>
+        <input id="knopka" type="submit" value="Register now">
+    </div>
+
+</body>
+</html>
+
+
+
+
+
+<?php
+php?>
