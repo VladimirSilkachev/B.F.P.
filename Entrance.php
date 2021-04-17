ab1e6866389a3c205755507cba9f01e9fb4efffe
KirillPopov001 <55420132+KirillPopov001@users.noreply.github.com>
2021-04-17 16:51:56 +0700

    Create Entrance.php

---
+++ Entrance.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<form id="head" action="check_entr.php" method="post" class="ramka">
+
+    <div>
+        <label id="login" for="login">Login</label>
+        <input type="text" name="login">
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input type="password" name="password">
+    </div>
+
+    <div>
+        <input id="knopka" type="submit" value="Entrance">
+    </div>
+</form>
+
+</body>
+</html>
