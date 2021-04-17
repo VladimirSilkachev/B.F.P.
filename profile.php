c45a586ade7f414dde3d4cfcc2daab5f808d3d10
SashaVin <55402145+SashaVin@users.noreply.github.com>
2021-04-17 21:55:04 +0700

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,20 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "user") or die("No connection");
+$query ="SELECT * FROM user";
+$result = mysqli_query($connect, $query) or die("No connection");
+echo "WELCOME, ";
+$login = $_COOKIE["Login"];
+echo $login;
+if($result)
+{
+    $rows = mysqli_num_rows($result);
+    for ($i = 0 ; $i < ($rows) ; ++$i)
+    {
+    $row = mysqli_fetch_row($result);
+    if ($row[1] === $login){
+    echo $row[1];
+    $info = array($row[3], $row[4], $row[5], $row[6], $row[7], $row[8]); // массив с данными профиля
+    }
+    }
+    }
+?>
