5e63b7fd97cfa6a059b904dbba395e20374c3b0b
SashaVin <55402145+SashaVin@users.noreply.github.com>
2021-04-17 18:48:34 +0700

    Update check_entr.php

--- check_entr.php
+++ check_entr.php
@@ -1,6 +1,48 @@
-<?
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<?php
 
 $login = $_POST['login'];
 $password = $_POST['password'];
 
+$connect = mysqli_connect("localhost", "root", "", "user") or die("No connection");
+$query ="SELECT * FROM user";
+$result = mysqli_query($connect, $query) or die("No connection");
+$logins = "";
+$passwords = "";
+if($result)
+{
+    $rows = mysqli_num_rows($result);
+    for ($i = 0 ; $i < ($rows) ; ++$i)
+    {
+    $row = mysqli_fetch_row($result);
+    $logins .= $row[1];
+    $passwords .= $row[2];
+    $logins .= " ";
+    $passwords .= " ";
+    }
+    }
+$l_array = explode(" ", $logins);
+$p_array = explode(" ", $passwords);
+$l = array_search ($login, $l_array);
+$p = array_search ($password, $p_array);
+if (in_array($login, $l_array)) {
+    if ($p !== false and $p === $l){
+        require("profile.php");
+    } else {
+        echo "Incorrect login or password";
+    }
+} else {
+    require("Entrance.php");
+    echo "Incorrect login or password";}
 ?>
+
+
+</body>
+</html>
