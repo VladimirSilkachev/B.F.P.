699e3975eafcff377bfb8b602bf43c8c6cc4fe03
KirillPopov001 <55420132+KirillPopov001@users.noreply.github.com>
2021-04-17 16:49:08 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+
+<a href="Entrance.php">Entrance</a><br>
+<a href="Registration.php">Registration</a><br>
+<a href="new_pass.php">Forgot password?</a><br>
+
+</body>
+</html>
