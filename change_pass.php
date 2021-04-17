0341b6f5d99c24e1235a12725f9c182c21414f7a
KirillPopov001 <55420132+KirillPopov001@users.noreply.github.com>
2021-04-17 16:54:10 +0700

    Create change_pass.php

---
+++ change_pass.php
@@ -0,0 +1,34 @@
+<?
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<form id="head" action="check_np.php" method="post" class="ramka">
+
+    <div>
+        <label id="login" for="password">New password</label>
+        <input type="password" name="password">
+    </div>
+
+    <div>
+        <label for="2_password">Confirm new password</label>
+        <input type="password" name="2_password">
+    </div>
+
+    <div>
+        <input id="knopka" type="submit" value="Change password">
+    </div>
+
+</body>
+</html>
